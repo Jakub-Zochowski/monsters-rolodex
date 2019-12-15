aef8b8587dd51845dd5267a8d0f4de007b3d665e
Jakub Zochowski <jakub.zochowski997@gmail.com>
2019-12-15 22:50:00 +0000

    Updates

---
+++ precache-manifest.5114936fe71a90752ee98f8019fb51e4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d6937b920f8b0386d9615b54a4c4160",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "0a807bbf0e3fdf519201",
+    "url": "/monsters-rolodex/static/css/main.4f8f37a2.chunk.css"
+  },
+  {
+    "revision": "5ffb6fab3c6fb8afc674",
+    "url": "/monsters-rolodex/static/js/2.f6373902.chunk.js"
+  },
+  {
+    "revision": "0a807bbf0e3fdf519201",
+    "url": "/monsters-rolodex/static/js/main.99b0ec7c.chunk.js"
+  },
+  {
+    "revision": "03497ef441aebc7cc47b",
+    "url": "/monsters-rolodex/static/js/runtime-main.666613d3.js"
+  }
+]);
